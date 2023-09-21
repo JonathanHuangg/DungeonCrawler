098d1a8dea4f16d09c6544aef517afa705443d73
Jessie Rigsbee <89279581+Jessierigsbee@users.noreply.github.com>
2023-09-21 18:43:18 -0400

    adding Start and Exit buttons

--- DungenCrawler/app/src/main/java/com/example/dungencrawler/WelcomeActivity.java
+++ DungenCrawler/app/src/main/java/com/example/dungencrawler/WelcomeActivity.java
@@ -2,13 +2,40 @@ package com.example.dungencrawler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
+    private Button button;
+    private Button extButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        button = (Button) findViewById(R.id.startButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openConfigurationScreen();
+            }
+        });
+
+        extButton = (Button) findViewById(R.id.exitButton);
+
+        extButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    public void openConfigurationScreen() {
+        Intent intent = new Intent(this, MainActivity.class); //this may need to change based on screen
+        startActivity(intent);
     }
 }
