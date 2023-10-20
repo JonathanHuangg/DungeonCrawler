94fdb99fd136f13cd40c1eb12d18171e36cb0312
Jessie Rigsbee <89279581+Jessierigsbee@users.noreply.github.com>
2023-10-20 14:10:21 -0400

    pattern changes

--- DungenCrawler/app/src/main/java/com/example/dungencrawler/model/PlayerMovementDown.java
+++ DungenCrawler/app/src/main/java/com/example/dungencrawler/model/PlayerMovementDown.java
@@ -1,10 +1,14 @@
 package com.example.dungencrawler.model;
 
-public class PlayerMovementDown implements EntityStrategy {
+public class PlayerMovementDown implements EntityStrategy, Subscriber {
     @Override
     public void execute(Player player) {
         if (player.getPlayerY() < 750) {
             player.setPlayerY(player.getPlayerY() + 20);
         }
+    }
+    @Override
+    public void update(Player player) {
+        execute(player);
     }
 }
--- DungenCrawler/app/src/main/java/com/example/dungencrawler/model/PlayerMovementRight.java
+++ DungenCrawler/app/src/main/java/com/example/dungencrawler/model/PlayerMovementRight.java
@@ -1,10 +1,14 @@
 package com.example.dungencrawler.model;
 
-public class PlayerMovementRight implements EntityStrategy {
+public class PlayerMovementRight implements EntityStrategy, Subscriber {
     @Override
     public void execute(Player player) {
         if (player.getPlayerX() < 2100) {
             player.setPlayerX(player.getPlayerX() + 20);
         }
+    }
+    @Override
+    public void update(Player player) {
+        execute(player);
     }
 }
