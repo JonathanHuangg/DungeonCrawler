4da01a0b380d595805c2c896b5be681af2edbcc1
Edison9138 <edison910308@gmail.com>
2023-10-20 15:04:29 -0400

    add 2 unit tests

--- DungenCrawler/app/src/androidTest/java/com/example/dungencrawler/CharacterMovementTest.java
+++ DungenCrawler/app/src/androidTest/java/com/example/dungencrawler/CharacterMovementTest.java
@@ -9,7 +9,7 @@ public class CharacterMovementTest {
     public void testCharMovementRight() {
         Player player1 = new Player("Thomas", 1000, 0, 0);
         player1.setEntityStrategy(new PlayerMovementRight());
-        player1.getEntityStrategy().execute(player1);
+        player1.getEntityStrategy().execute(player1, 0, 0);
         assertEquals(player1.getPlayerX(), 20, 0.000001);
     }
 
@@ -17,7 +17,7 @@ public class CharacterMovementTest {
     public void testCharMovementDown() {
         Player player2 = new Player("Edison", 2000, 0, 0);
         player2.setEntityStrategy(new PlayerMovementRight());
-        player2.getEntityStrategy().execute(player2);
+        player2.getEntityStrategy().execute(player2, 0, 0);
         assertEquals(player2.getPlayerX(), 20, 0.000001);
     }
 }
