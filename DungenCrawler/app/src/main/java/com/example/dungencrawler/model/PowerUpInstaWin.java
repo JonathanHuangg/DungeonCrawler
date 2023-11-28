a7107fcee0130e01f09e28d1398f22c4ad178775
Jonathan Huang <jonathannhuang@gmail.com>
2023-11-27 20:43:37 -0500

    fixed up PowerUpView, added an interface called PowerUp which updates the three specific powerups

--- DungenCrawler/app/src/main/java/com/example/dungencrawler/model/PowerUpInstaWin.java
+++ DungenCrawler/app/src/main/java/com/example/dungencrawler/model/PowerUpInstaWin.java
@@ -1,20 +1,18 @@
-
 package com.example.dungencrawler.model;
 
+public class PowerUpInstaWin extends PowerUpDecorator implements PowerUp {
 
-public class PowerUpHealth extends PowerUpDecorator implements PowerUp {
     private float x;
     private float y;
-    public PowerUpHealth(PlayerInterface decoratedPlayer, float x, float y) {
+    public PowerUpInstaWin(PlayerInterface decoratedPlayer, float x, float y) {
         super(decoratedPlayer);
         this.x = x;
         this.y = y;
     }
 
     @Override
-    public void setHealth(int health) {
-        // Enhance the setHealth behavior
-        super.setHealth(health + 100);
+    public void setPlayerWinResult(int score) {
+        super.setPlayerWinResult(100);
     }
 
     public float getX() {
--- DungenCrawler/app/src/main/java/com/example/dungencrawler/viewmodels/PowerUpView.java
+++ DungenCrawler/app/src/main/java/com/example/dungencrawler/viewmodels/PowerUpView.java
@@ -5,26 +5,63 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.view.View;
-import com.example.dungencrawler.model.PowerUpSlashAndDash;
-public class PowerUpView extends View {
-    private PowerUpSlashAndDash powerUp; //I am doing this one first
-    private Bitmap powerUpBitmap;
 
-    public PowerUpView(Context context, PowerUpSlashAndDash powerUp, int powerUpResourceId) {
+import com.example.dungencrawler.model.PowerUpHealth;
+import com.example.dungencrawler.model.PowerUpInstaWin;
+import com.example.dungencrawler.model.PowerUpSlashAndDash;
+
+public class PowerUpView extends View {
+    private final PowerUpSlashAndDash powerUpSlash;
+    private final PowerUpInstaWin powerUpInsta;
+    private final PowerUpHealth powerUpHealth;
+
+    private Bitmap powerUpSlashBitmap;
+    private Bitmap powerUpInstaBitmap;
+    private Bitmap powerUpHealthBitmap;
+
+
+    public PowerUpView(Context context, PowerUpSlashAndDash powerUpSlash, int powerUpSlashResourceId,
+                       PowerUpInstaWin powerUpInsta, int powerUpInstaResourceId,
+                       PowerUpHealth powerUpHealth, int powerUpHealthResourceId) {
         super(context);
-        this.powerUp = powerUp;
-        this.powerUpBitmap = BitmapFactory.decodeResource(getResources(), powerUpResourceId);
+        this.powerUpSlash = powerUpSlash;
+        this.powerUpInsta = powerUpInsta;
+        this.powerUpHealth = powerUpHealth;
+
+        this.powerUpSlashBitmap = BitmapFactory.decodeResource(getResources(), powerUpSlashResourceId);
+        this.powerUpInstaBitmap = BitmapFactory.decodeResource(getResources(), powerUpInstaResourceId);
+        this.powerUpHealthBitmap = BitmapFactory.decodeResource(getResources(), powerUpHealthResourceId);
+
         int width = 100;
         int height = 100;
-        this.powerUpBitmap = Bitmap.createScaledBitmap(powerUpBitmap, width, height, true);
+        this.powerUpSlashBitmap = Bitmap.createScaledBitmap(powerUpSlashBitmap, width, height, true);
+        this.powerUpInstaBitmap = Bitmap.createScaledBitmap(powerUpInstaBitmap, width, height, true);
+        this.powerUpHealthBitmap = Bitmap.createScaledBitmap(powerUpHealthBitmap, width, height, true);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        if (powerUpBitmap != null) {
-            // Assuming PowerUpSlashAndDash has getX() and getY() methods
-            canvas.drawBitmap(powerUpBitmap, powerUp.getX(), powerUp.getY(), null);
+        drawPowerUpSlash(canvas, powerUpSlash, powerUpSlashBitmap);
+        drawPowerUpInsta(canvas, powerUpInsta, powerUpInstaBitmap);
+        drawPowerUpHealth(canvas, powerUpHealth, powerUpHealthBitmap);
+    }
+
+    private void drawPowerUpHealth(Canvas canvas, PowerUpHealth powerUp, Bitmap bitmap) {
+        if (powerUp != null && bitmap != null) {
+            canvas.drawBitmap(bitmap, powerUp.getX(), powerUp.getY(), null);
+        }
+    }
+
+    private void drawPowerUpSlash(Canvas canvas, PowerUpSlashAndDash powerUp, Bitmap bitmap) {
+        if (powerUp != null && bitmap != null) {
+            canvas.drawBitmap(bitmap, powerUp.getX(), powerUp.getY(), null);
+        }
+    }
+
+    private void drawPowerUpInsta(Canvas canvas, PowerUpInstaWin powerUp, Bitmap bitmap) {
+        if (powerUp != null && bitmap != null) {
+            canvas.drawBitmap(bitmap, powerUp.getX(), powerUp.getY(), null);
         }
     }
 }
